037db4054898b81e85beb456068038f281f6e69b
Daniellymoreira <59980463+dany679@users.noreply.github.com>
2025-11-04 22:01:55 -0300

    feat(monorepo): setup

--- apps/web/app/page.js
+++ apps/web/app/page.js
@@ -1,14 +1,9 @@
-import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
-
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
+    return (<div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
-        {add(2, 3)}
       </div>
-    </div>
-  );
+    </div>);
 }
